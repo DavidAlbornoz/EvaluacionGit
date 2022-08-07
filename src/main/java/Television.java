47c68d419ff402d53f55321c0a92759c7b374b07
David <no.david.albornoz@gmail.com>
2022-08-07 02:16:28 -0500

    add Television

---
+++ src/main/java/Television.java
@@ -0,0 +1,10 @@
+public class Television {
+    private double resolucion;
+    private boolean sintonizadorTDT;
+
+    public Television(){
+        this.resolucion = 20;
+        this.sintonizadorTDT = false;
+    }
+
+}
